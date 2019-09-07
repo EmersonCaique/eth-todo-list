25f042b5a81b21c58c56c4fdc2f107851fb2fe71
Emerson Caique <emerson.caique@outlook.com>
2019-09-07 15:17:33 -0300

    List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,7 +2,22 @@ pragma solidity ^0.5.0;
 
 
 contract TodoList {
-    // state variable
-    //REPRESENTA O ESTADO NA BLOCKCHAIN
-    uint public taskCount = 0; 
+    uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    } 
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Comprar pão.");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false); 
+    }
 }
